708bb18cdb5a5afde1e6d8fc1e1ca957126c261c
GarveyWu <102859450+GarveyPython@users.noreply.github.com>
2024-06-04 17:01:02 +0800

    Update program.py

--- program.py
+++ program.py
@@ -1,145 +1,104 @@
-import random
-import math
-import copy
-import matplotlib.pyplot as plt
-
-plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
-color = ['blue', 'purple', 'green', 'red', 'lawngreen', 'yellow', 'pink', 'brown', 'orange', 'cyan', 'magenta', 'teal', 'coral']
-
-
-# 计算完工时间的函数
-def cal_makespan(data, dispatch_lst, render=False):
-    if render:
-        plt.figure(figsize=(20, 8), dpi=300)
-        x_tricks = []
-        x_tricks.append(0)
-        y_tricks = [f"M{j}" for j in range(1, len(data[0]) + 1)]
-        y_tricks.insert(0, "")
-        plt.yticks([j for j in range(len(data[0]) + 1)], y_tricks)
-    # 工件的开始加工时间列表
-    start_time_lst = [0 for _ in range(len(dispatch_lst))]
-    # 工件的结束加工时间列表
-    end_time_lst = [0 for _ in range(len(dispatch_lst))]
-    for j in range(len(data[0])):
-        for i in range(len(dispatch_lst)):
-            if j == 0:
-                start_time_lst[dispatch_lst[i]] = end_time_lst[dispatch_lst[i - 1]] if i > 0 else 0
-                end_time_lst[dispatch_lst[i]] = start_time_lst[dispatch_lst[i]] + data[dispatch_lst[i]][j]
-                if render:
-                    plt.barh(j + 1, data[dispatch_lst[i]][j], height=1, left=start_time_lst[dispatch_lst[i]],
-                             align='center', color=color[dispatch_lst[i]], edgecolor='grey')
-            else:
-                tmp_start_point = max(end_time_lst[dispatch_lst[i]], end_time_lst[dispatch_lst[i - 1]]) if i > 0 else \
-                end_time_lst[dispatch_lst[i]]
-                end_time_lst[dispatch_lst[i]] = tmp_start_point + data[dispatch_lst[i]][j]
-                if render:
-                    plt.barh(j + 1, data[dispatch_lst[i]][j], height=1, left=tmp_start_point, align='center',
-                             color=color[dispatch_lst[i]], edgecolor='grey')
-                    if j==len(data[0])-1:
-                        plt.text(tmp_start_point+5, j + 1, f"J{dispatch_lst[i] + 1}")
-    cmax = max(end_time_lst)
-    if render:
-        plt.title('调度甘特图')
-        x_tricks.append(cmax)
-        plt.xticks(x_tricks)
-        plt.axvline(cmax, color='r', linestyle='--')
-        plt.show()
-        print(start_time_lst)
-        print(end_time_lst)
-    return cmax
-
-
-# 随机生成一个初始解
-def preDispatch(data):
-    pre_dispatch_lst = [i for i in range(len(data))]
-    random.shuffle(pre_dispatch_lst)
-    return pre_dispatch_lst, cal_makespan(data, pre_dispatch_lst)
-
-
-# 交换函数
-def swap(a, b):
-    temp = a
-    a = b
-    b = temp
-    return a, b
-
-
-# 局部搜索函数(两位置交换排序法)
-def localSearch(data, dispatch_lst):
-    for t in range(len(dispatch_lst) - 1):
-        for i in range(t + 1, len(dispatch_lst)):
-            tmp_dispatch_lst = copy.deepcopy(dispatch_lst)
-            tmp_dispatch_lst[t], tmp_dispatch_lst[i] = swap(tmp_dispatch_lst[t], tmp_dispatch_lst[i])
-            if cal_makespan(data, tmp_dispatch_lst) < cal_makespan(data, dispatch_lst):
-                dispatch_lst = tmp_dispatch_lst
-    return dispatch_lst, cal_makespan(data, dispatch_lst)
-
-
-# 更新温度的函数
-def update_temperature(base_T, anneal_F, set_iteration, iteration):
-    return base_T * math.exp(-anneal_F * (set_iteration - iteration))
-
-
-# 邻域搜索函数
-def reverseSubsequence(data, dispatch_lst):
-    i, j = sorted(random.sample(range(len(dispatch_lst)), 2))
-    new_dispatch_lst = dispatch_lst[:i] + dispatch_lst[i:j + 1][::-1] + dispatch_lst[j + 1:]
-    new_makespan = cal_makespan(data, new_dispatch_lst)
-    return new_dispatch_lst, new_makespan
-
-
-# 全局搜索函数(模拟退火法)
-def globalSearch(data, dispatch_lst, makespan, base_T, anneal_F, iteration):
-    x_lst = [i for i in range(1, iteration + 1)]
-    y_lst = []
-    set_base_T = base_T
-    set_iteration = iteration
-    while iteration > 0:
-        get_dispatch_lst, get_makespan = reverseSubsequence(data, dispatch_lst)
-        if get_makespan <= makespan:
-            # 局部搜索
-            up_dispatch_lst, up_makespan = localSearch(data, get_dispatch_lst)
-            # 更新最优调度方案
-            dispatch_lst, makespan = up_dispatch_lst, up_makespan
-        else:
-            delta_f = makespan - get_makespan
-            temperature = update_temperature(set_base_T, anneal_F, set_iteration, iteration)
-            cal_p = math.exp(delta_f / temperature)
-            rand_number = random.random()
-            # 以一定概率选择劣解
-            if cal_p > rand_number:
-                # 局部搜索
-                up_dispatch_lst, up_makespan = localSearch(data, get_dispatch_lst)
-                if up_makespan < makespan:
-                    # 更新最优调度方案
-                    dispatch_lst, makespan = up_dispatch_lst, up_makespan
-        y_lst.append(makespan)
-        iteration -= 1
-    plt.plot(x_lst, y_lst, linewidth=1, linestyle="solid")
-    plt.text(set_iteration - 1, makespan + 10, str(makespan), fontsize=10, color='black')
-    plt.title('算法迭代图')
-    plt.xlabel('迭代次数')
-    plt.ylabel('最大完工时间')
-    plt.show()
-    return dispatch_lst, makespan
-
-
-if __name__ == '__main__':
-    # Car2 13x2 7166
-    data = [[654, 147, 345, 447],
-            [321, 520, 789, 702],
-            [12, 147, 630, 255],
-            [345, 586, 214, 866],
-            [678, 532, 275, 332],
-            [963, 145, 302, 225],
-            [25, 24, 142, 589],
-            [874, 517, 24, 996],
-            [114, 896, 520, 541],
-            [785, 543, 336, 234],
-            [203, 210, 699, 784],
-            [696, 784, 855, 512],
-            [302, 512, 221, 345]]
-    preDispatchList, makespan = preDispatch(data)
-    res_DispatchList, res_makespan = globalSearch(data, preDispatchList, makespan, 100, 0.02, 40)
-    print(res_DispatchList, res_makespan)
-    cal_makespan(data, res_DispatchList, render=True)
+import random  
+import math  
+import copy  
+import time  
+random.seed(1)  
+import matplotlib.pyplot as plt  
+from get_data import get_data  
+from solution import Solution  
+from fuzzy_operators import rank, value  
+  
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文  
+  
+  
+# 交换函数  
+def swap(a, b):  
+    temp = a  
+    a = b  
+    b = temp  
+    return a, b  
+  
+  
+# 局部搜索函数(两位置交换排序法)  
+def localSearch(solution, dispatch_lst, makespan):  
+    first_dispatch_lst = dispatch_lst[0][0][0]  
+    for t in range(len(first_dispatch_lst) - 1):  
+        for i in range(t + 1, len(first_dispatch_lst)):  
+            tmp_first_dispatch_lst = copy.deepcopy(first_dispatch_lst)  
+            tmp_first_dispatch_lst[t], tmp_first_dispatch_lst[i] = swap(tmp_first_dispatch_lst[t],  
+                                                                        tmp_first_dispatch_lst[i])  
+            tmp_encoder, tmp_obj = solution.decode(tmp_first_dispatch_lst)  
+            if rank(tmp_obj, makespan) == makespan:  
+                dispatch_lst = tmp_encoder  
+                makespan = tmp_obj  
+    return dispatch_lst, makespan  
+  
+  
+# 更新温度的函数  
+def update_temperature(base_T, anneal_F, set_iteration, iteration):  
+    return base_T * math.exp(-anneal_F * (set_iteration - iteration))  
+  
+  
+# 邻域搜索函数  
+def reverseSubsequence(solution, dispatch_lst):  
+    first_dispatch_lst = dispatch_lst[0][0][0]  
+    i, j = sorted(random.sample(range(len(first_dispatch_lst)), 2))  
+    new_first_dispatch_lst = first_dispatch_lst[:i] + first_dispatch_lst[i:j + 1][::-1] + first_dispatch_lst[j + 1:]  
+    new_dispatch_lst, new_makespan = solution.decode(new_first_dispatch_lst)  
+    return new_dispatch_lst, new_makespan  
+  
+  
+# 全局搜索函数(模拟退火法)  
+def globalSearch(solution, base_T, anneal_F, iteration,begin):  
+    dispatch_lst = solution.encoder  
+    makespan = solution.obj  
+    x_lst = [i for i in range(1, iteration + 1)]  
+    y_lst = []  
+    set_base_T = base_T  
+    set_iteration = iteration  
+    flag = 1  
+    while iteration > 0:  
+        get_dispatch_lst, get_makespan = reverseSubsequence(solution, dispatch_lst)  
+        if rank(get_makespan, makespan) == makespan:  
+            # 局部搜索  
+            up_dispatch_lst, up_makespan = localSearch(solution, dispatch_lst, makespan)  
+            # 更新最优调度方案  
+            dispatch_lst, makespan = up_dispatch_lst, up_makespan  
+        else:  
+            delta_f = value(makespan) - value(get_makespan)  
+            temperature = update_temperature(set_base_T, anneal_F, set_iteration, iteration)  
+            cal_p = math.exp(delta_f / temperature)  
+            rand_number = random.random()  
+            # 以一定概率选择劣解  
+            if cal_p > rand_number:  
+                # 局部搜索  
+                up_dispatch_lst, up_makespan = localSearch(solution, dispatch_lst, makespan)  
+                if rank(up_makespan, makespan) == makespan:  
+                    # 更新最优调度方案  
+                    dispatch_lst, makespan = up_dispatch_lst, up_makespan  
+        y_lst.append(value(makespan))  
+        iteration -= 1  
+        print(f"iteration{flag} fuzzy makespan{makespan}")  
+        flag += 1  
+    print(f"算法共运行{time.time() - begin}s")  
+    plt.plot(x_lst, y_lst, linewidth=1, linestyle="solid")  
+    plt.text(set_iteration - 1, value(makespan) + 10, str(int(value(makespan))), fontsize=10, color='black')  
+    plt.title('算法迭代图')  
+    plt.xlabel('迭代次数')  
+    plt.ylabel('最大模糊完工时间')  
+    plt.show()  
+    solution.encoder = dispatch_lst  
+    solution.obj = makespan  
+    solution.draw_fuzzy_gantt()  
+    return dispatch_lst, makespan  
+  
+  
+if __name__ == '__main__':  
+    inst = 'data/k6_l6_C_t5.json'  # Extract from Taillard50x20_5  
+    job_num, machine_num, stage_num, layer_num, layer_stage_list, machines, layer_ava_machines, new_release_list = get_data(  
+        inst)  
+    release_code = 'interval100_q5'  
+    release_dict = new_release_list[release_code]  
+    begin = time.time()  
+    solution = Solution(job_num, stage_num, machine_num, layer_stage_list, layer_ava_machines, machines, release_dict)  
+    res_DispatchList, res_makespan = globalSearch(solution, 100, 0.02, 100, begin)  
